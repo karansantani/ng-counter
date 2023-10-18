02d4b4c182d18ca06aef52eb47f85c8175f10d49
Karan <karansantani@gmail.com>
2023-10-18 10:52:27 +0530

    Counter app using Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'ng-counter';
+  counter: number[] = [];
+  initializer: number = 0;
+  incrementer: number = 1;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.counter.push(this.initializer);
+  }
+
+  increment() {
+    this.counter.push(this.counter[this.counter.length - 1] + this.incrementer);
+  }
+  
+  decrement() {
+    this.counter.push(this.counter[this.counter.length - 1] - this.incrementer);
+  }
+
+  reset() {
+    this.counter = [];
+    this.counter.push(this.initializer);
+  }
+
+  restore() {
+    this.counter.pop();
+    if (this.counter.length === 0) {
+      this.counter.push(this.initializer);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FormsModule } from '@angular/forms';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatRippleModule } from '@angular/material/core';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [
@@ -10,7 +22,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatRippleModule,
+    MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
